67fcf670821ae5a7ca955991971cafa26dd28fd0
alexandre nesovic <alexandrenesovic@gmail.com>
2020-08-26 14:37:35 +0200

    mgmt

--- app.py
+++ app.py
@@ -27,6 +27,17 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "mysecretkey"
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+login_manager.init__app(app)
+login_manager.login_view = 'login'
 
 
 @app.route('/')
