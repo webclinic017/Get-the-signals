883ac001257cca78bdc5bb3dfbf1ae93b6fdfac9
alexandre nesovic <alexandrenesovic@gmail.com>
2020-08-26 10:10:47 +0200

    cleaning

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, CustomJS, Slider
 from bokeh.embed import components
+from bokeh.models import DataRange1d
 
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 
 from sqlite_utils import from_csv_to_sqlite3
 
@@ -61,11 +66,36 @@ def graph():
     return render_template('graph.html', **kwargs)
 
 
+# to add https://docs.bokeh.org/en/latest/docs/gallery/bar_mixed.html
+# to add https://docs.bokeh.org/en/latest/docs/gallery/bar_nested_colormapped.html
 @app.route('/bar')
 def bar():
-    bar_labels = labels
-    bar_values = values
-    return render_template('bar_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=bar_labels, values=bar_values)
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    # https://github.com/realpython/flask-bokeh-example/blob/master/tutorial.md
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'bar.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
 
 
 @app.route('/table')
