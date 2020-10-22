0f62b302b9467ce95488b6b1c94ee7b9beddda70
alexnesov <alexandrenesovic@gmail.com>
2020-10-22 16:18:58 +0200

    del bokeh + adding MySQL connector

--- SV/__init__.py
+++ SV/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+db_user = os.environ.get('aws_db_user')
+db_pass = os.environ.get('aws_db_pass')
+db_endpoint = os.environ.get('aws_db_endpoint')
 
 # Create a login manager object
 login_manager = LoginManager()
@@ -13,8 +16,11 @@ app = Flask(__name__)
 # Often people will also separate these into a separate config.py file
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'data.sqlite')
+""" app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite') """
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_pass}@{db_endpoint}:3306/flaskfinance'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- app.py
+++ app.py
@@ -4,17 +4,9 @@ from flask_login import login_user, login_required, logout_user
 from SV.models import User
 from SV.forms import LoginForm, RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
 from SV.sqlite_utils import from_csv_to_sqlite3
-from bokeh.util.string import encode_utf8
-from bokeh.resources import INLINE
-from bokeh.models import DataRange1d
-from bokeh.embed import components
-from bokeh.models import ColumnDataSource, CustomJS, Slider
-from bokeh.layouts import column
-from bokeh.plotting import figure, output_file, show
-
-import sqlite3
 
 
 @app.route('/')
@@ -83,61 +75,6 @@ def register():
     return render_template('register.html', form=form)
 
 
-@app.route('/graph')
-@login_required
-def graph():
-    x = [x*0.005 for x in range(0, 200)]
-    y = x
-
-    source = ColumnDataSource(data=dict(x=x, y=y))
-
-    plot = figure(plot_width=400, plot_height=400)
-    plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
-    callback = CustomJS(args=dict(source=source), code="""
-            var data = source.data;
-            var f = cb_obj.value
-            var x = data['x']
-            var y = data['y']
-            for (var i = 0; i < x.length; i++) {
-                y[i] = Math.pow(x[i], f)
-            }
-            source.change.emit();
-        """)
-
-    slider = Slider(start=0.1, end=4, value=1, step=.1, title="power")
-    slider.js_on_change('value', callback)
-
-    layout = column(slider, plot)
-    script, (plot_div, slider_div) = components((plot, column(slider)))
-    kwargs = {'script': script, 'plot_div': plot_div, 'slider_div': slider_div}
-    kwargs['title'] = 'bokeh-with-flask'
-
-    return render_template('graph.html', **kwargs)
-
-
-@app.route('/multiple')
-@login_required
-def multiple():
-    title = 'multiple'
-    from bokeh.plotting import figure
-
-    # First Plot
-    p = figure(plot_width=400, plot_height=400)
-    p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5],
-             size=20, color="navy", alpha=0.5)
-
-    # Second Plot
-    p2 = figure(plot_width=400, plot_height=400)
-    p2.square([5, 8, 3, 4, 5], [20, 7, 2, 1, 25],
-              size=20, color="red", alpha=0.5)
-
-    script, div = components(p)
-    script2, div2 = components(p2)
-
-    return render_template('multiple.html', title=title, script=script,
-                           div=div, script2=script2, div2=div2)
-
 
 @app.route('/table')
 @login_required
@@ -164,4 +101,4 @@ def table():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
