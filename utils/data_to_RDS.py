edabeedfd78a66c73770b059bda1fa42ea607a36
alexnesov <alexandrenesovic@gmail.com>
2020-10-25 15:44:36 +0100

    load ok

--- utils/data_to_RDS.py
+++ utils/data_to_RDS.py
@@ -8,23 +8,22 @@ db_endp = os.environ.get('aws_db_endpoint')
 
 
 
-db = pymysql.connect(f'{db_endp}',f'{db_user}',f'{db_pass}','flaskfinance')
+db = pymysql.connect(host=f'{db_endp}',user=f'{db_user}',password=f'{db_pass}',database='flaskfinance',local_infile=True)
 cursor = db.cursor()
 
-query = "LOAD DATA LOCAL INFILE 'Overview.csv'\
-        INTO TABLE financetest\
-        COLUMNS TERMINATED BY ','\
-        OPTIONALLY ENCLOSED BY '"'\
-        ESCAPED BY '"'\
-        LINES TERMINATED BY '\n'\
-        IGNORE 1 LINES\
-        (no, ticker, company, sector, industry, country, @market_cap, @p_e, price, change_, volume)\
-        SET\
-        market_cap = NULLIF(@market_cap,''),\
-        p_e = NULLIF(@p_e,'');"
-
+query = """
+LOAD DATA LOCAL INFILE 'Overview.csv'
+INTO TABLE financetest
+COLUMNS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES\
+(no, ticker, company, sector, industry, country, market_cap, p_e, price, change_, volume);
+"""
 cursor.execute(query)
-
+cursor.execute("UPDATE financetest SET market_cap = NULLIF(market_cap, ''), p_e = NULLIF(p_e, '');")
+db.commit()
 cursor.close()
 db.close()
 
