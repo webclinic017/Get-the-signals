8d5b7506727680079e330a4c123e60c3885506ae
alexnesov <alexandrenesovic@gmail.com>
2020-10-25 12:52:37 +0000

    dataToRDS

--- utils/data_to_RDS.py
+++ utils/data_to_RDS.py
@@ -11,7 +11,17 @@ db_endp = os.environ.get('aws_db_endpoint')
 db = pymysql.connect(f'{db_endp}',f'{db_user}',f'{db_pass}','flaskfinance')
 cursor = db.cursor()
 
-query = "LOAD DATA LOCAL INFILE '/home/ubuntu/financials-downloader-bot/Overview.csv'INTO TABLE financetest COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
+query = "LOAD DATA LOCAL INFILE 'Overview.csv'\
+        INTO TABLE financetest\
+        COLUMNS TERMINATED BY ','\
+        OPTIONALLY ENCLOSED BY '"'\
+        ESCAPED BY '"'\
+        LINES TERMINATED BY '\n'\
+        IGNORE 1 LINES\
+        (no, ticker, company, sector, industry, country, @market_cap, @p_e, price, change_, volume)\
+        SET\
+        market_cap = NULLIF(@market_cap,''),\
+        p_e = NULLIF(@p_e,'');"
 
 cursor.execute(query)
 
