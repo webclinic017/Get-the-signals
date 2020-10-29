31c2924d730e6e1d34565c38545ee45500446cb6
alexnesov <alexandrenesovic@gmail.com>
2020-10-29 19:28:46 +0100

    db name

--- utils/ec2_to_rds.py
+++ utils/ec2_to_rds.py
@@ -13,7 +13,7 @@ db_endp = os.environ.get('aws_db_endpoint')
 db = pymysql.connect(f'{db_endp}',f'{db_user}',f'{db_pass}','flaskfinance')
 cursor = db.cursor()
 
-query = "CREATE TABLE financetest (no INT(5), ticker VARCHAR(10) PRIMARY KEY, company VARCHAR(200),\
+query = "CREATE TABLE usStocksOverview (no INT(5), ticker VARCHAR(10) PRIMARY KEY, company VARCHAR(200),\
 sector VARCHAR(90), industry VARCHAR(250),country VARCHAR(30), market_cap FLOAT(25) DEFAULT NULL,\
 p_e FLOAT(8) DEFAULT NULL,price FLOAT(8), change_ FLOAT(8), volume INT(10));"
 cursor.execute(query)
