c779eedec088a7321b376bd0749ce195e7da1c68
alexnesov <alexandrenesovic@gmail.com>
2020-10-31 18:23:59 +0100

    static init

--- SV/__init__.py
+++ SV/__init__.py
@@ -11,7 +11,7 @@ db_endpoint = os.environ.get('aws_db_endpoint')
 # Create a login manager object
 login_manager = LoginManager()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Often people will also separate these into a separate config.py file
 app.config['SECRET_KEY'] = 'mysecretkey'
--- app.py
+++ app.py
@@ -111,6 +111,6 @@ def table():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,static_url_path='SV/static')
+    app.run(debug=True)
 
 # https://stackoverflow.com/questions/15077489/static-assets-dont-show-up-for-flask-on-elastic-beanstalk
