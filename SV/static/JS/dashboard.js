e6a9e618f6e4c156412e1abe5e9740c1856ecec3
alexnesov <alexandrenesovic@gmail.com>
2020-11-09 20:49:50 +0100

    new sidebar

--- SV/static/JS/dashboard.js
+++ SV/static/JS/dashboard.js
@@ -1,14 +1,3 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-
-function openNav() {
-    document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
-
-
+function toggleSidebar(ref){
+  document.getElementById("sidebar").classList.toggle('active');
+}
