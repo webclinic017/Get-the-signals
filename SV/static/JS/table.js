c73f373958157eeb6cc3e70b4427eb8a57f6bbb9
alexnesov <alexandrenesovic@gmail.com>
2021-08-08 00:21:06 +0200

    v2

--- SV/static/JS/table.js
+++ SV/static/JS/table.js
@@ -1,1 +1,197 @@
-window.location.hash="signal_chart_section";
+
+// Once Dom is created & elements are rendered then run script to show plot.
+
+function getTS(){
+    d = new Date();
+    datetext = d.toTimeString();
+    datetext = datetext.split(' ')[0];
+    return datetext;
+}
+
+function createToast(graphID, textStatus, errorThrown) {
+    datetext = getTS();
+
+    toastNotifsContainer = document.getElementById("toastNotifsContainer");
+    toast = document.createElement("div");
+
+    toastAttrs = {
+        "class": "toast",
+        "role": "alert",
+        "aria-live": "assertive",
+        "aria-atomic": "true",
+        "data-bs-delay": "7000"
+    }
+
+    for ([k, v] of Object.entries(toastAttrs)) {
+        toast.setAttribute(k, v);
+    }
+
+    toastNotifsContainer.appendChild(toast);
+
+    toastHeader = document.createElement("div");
+    toast.appendChild(toastHeader);
+
+    toastHeader.className = "toast-header";
+    toastHeaderSVG = document.createElementNS('http://www.w3.org/2000/svg', "svg");
+    toastHeader.appendChild(toastHeaderSVG);
+
+    svgAttrs = {
+        "xmlns": "http://www.w3.org/2000/svg", "width": "16", "height": "16", "fill": "red",
+        "class": "bi bi-exclamation-triangle-fill", "viewBox": "0 0 16 16"
+    }
+
+    for ([k, v] of Object.entries(svgAttrs)) {
+        toastHeaderSVG.setAttribute(k, v);
+    }
+
+    svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    svgPath.setAttributeNS(null, "d", "M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z");
+
+    toastHeaderSVG.appendChild(svgPath);
+
+    strg = document.createElement("strong");
+    strg.setAttribute("class", "me-auto");
+    strg.innerHTML = "Error"
+
+    toastHeader.appendChild(strg);
+
+    sml = document.createElement("small");
+    sml.setAttribute("class", "text-muted");
+    sml.innerHTML = datetext;
+
+    toastHeader.appendChild(sml);
+
+    btn = document.createElement("button");
+    btnAttrs = { "class": "btn-close", "type": "button", "data-bs-dismiss": "toast", "aria-label": "Close" };
+
+    for ([k, v] of Object.entries(btnAttrs)) {
+        btn.setAttribute(k, v);
+    }
+
+    toastHeader.appendChild(btn);
+
+    toastBody = document.createElement("div");
+    toastBody.setAttribute("class", "toast-body");
+    toastBody.innerHTML = "Error: " + graphID + "\n" + textStatus + "\n" + errorThrown;
+    toast.appendChild(toastBody);
+
+    new bootstrap.Toast(toast).show();
+
+}
+
+function plotGraph(graphID, jsonData) {
+    // update the plot, .react is recommended for updating existing plot as it's more efficient than .newPlot
+    Plotly.react(graphID, jsonData, { autosize: true });
+}
+
+
+// DRY
+function fetchGraphData(apiURL, graphID, spinnerID, refreshBtnID, tsID) {
+    // jQuery.get( url [, data ] [, success ] [, dataType ] )
+    // $.get({
+    //     url: apiURL,
+    //     data: null,
+    //     success: function (response) {
+    //         console.log("response received from server");
+    //         plotGraph(graphID, response);
+    //         console.log("ploting graph");
+    //     },
+    //     dataType: "json"
+    // });
+
+
+    $.ajax({
+        type: 'GET',
+        url: apiURL,
+        data: null,
+        beforeSend: function () {
+            console.log("starting spinner")
+            $("#" + spinnerID).addClass("spinner-border");
+            $("#" + refreshBtnID).hide();
+        },
+        success: function (response) {
+            console.log("response received from server");
+            plotGraph(graphID, JSON.parse(response));
+            console.log("ploting graph");
+            $("#"+tsID).text(getTS());
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            createToast(graphID, textStatus, errorThrown);
+            console.log("Error:", graphID, textStatus, errorThrown);
+
+            //errToast = bootstrap.Toast.getOrCreateInstance("#errorToast", {"delay":7000});
+            // errToast = new bootstrap.Toast("#errorToast", {"delay":7000})
+            // errToast.show();
+        },
+        complete: function () {
+            $("#" + spinnerID).removeClass("spinner-border");
+            $("#" + refreshBtnID).show();
+        }
+
+    }); //close ajax
+}
+
+
+
+function fetchSignalChartJsonData() {
+    // Graph_2
+    console.log("sending request for > signalChart < to server");
+    
+    fetchGraphData(`http://0.0.0.0:5000/api/fetchSignalChartJsonData?tick=${jQuery('#inputTick').val().trim()}`, "signalChart", "loader_graph2", "refreshBtn_2", "graph_2_TS");
+
+}
+
+
+// Initial Plots
+Plotly.newPlot("signalChart", null, { autosize: true }, { responsive: true }); // Graph 2
+fetchSignalChartJsonData(); // inital page load graph 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // window.onresize = function () {
+        //     ["barGraph", "signalChart", "temp"].forEach(graphID,
+        //         Plotly.relayout(graphID, {
+        //             'xaxis.autorange': true,
+        //             'yaxis.autorange': true
+        //         }));
+        // };
+
+        // setInterval(fetchGraphJsonData, 5000); // update every 5 seconds 
+
+
+        // Understand promise concepts <- Javascript
+
+        // fetch('http://127.0.0.1:5000/fetchGraphJsonData')
+        // .then(response => response.json())
+        // .then(function(jsonData){
+        //     console.log(jsonData);
+        //     plotGraph(jsonData);
+        // }());
+
+        // function showAllToast() {
+        //     // experimental - not debugged completely
+        //     var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+        //     var toastList = toastElList.map(function (toastEl) {
+        //         return new bootstrap.Toast(toastEl)
+        //     });
+        //     toastList.forEach(toast => toast.show());
+        //     console.log(toastList);
+        // }
+
+
--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import render_template, Response, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user
-from numpy.core.fromnumeric import std
 from wtforms import TextField, Form
 import os
 import json
@@ -13,6 +12,12 @@ from SV.forms import LoginForm, RegistrationForm
 from utils.db_manage import std_db_acc_obj
 from utils.fetchData import fetchSignals, fetchTechnicals, fetchOwnership, sp500evol
 from utils.graphs import makeLinesSignal, makeOwnershipGraph, lineNBSignals
+
+
+import plotly
+from plotly.subplots import make_subplots
+from utils.db_manage import QuRetType, std_db_acc_obj
+import plotly.graph_objs as go
 
 
 strToday = str(datetime.today().strftime('%Y-%m-%d'))
@@ -112,26 +117,6 @@ def login():
     return render_template('login.html', form=form)
 
 
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-
-
-@app.route('/rtvs')
-@login_required
-def rtvs():
-    return render_template('rtvs.html')
-
-
-@app.route('/portfolios')
-@login_required
-def portfolios():
-    return render_template('portfolios.html')
-
-
 ####------Standard functions and arguments for the table page------#
 
 
@@ -140,7 +125,6 @@ colNames = ['ValidTick','SignalDate',
 'PriceAtSignal', 'Last closing price',
 'Price Evolution', 'Company',
 'Sector','Industry']
-
 
 
 
@@ -184,34 +168,17 @@ def table():
     """
     Standard view for the "table" page
     """
-
     standard_args_table_page = STD_FUNC_TABLE_PAGE()
-    SignalChart = makeLinesSignal(tick='AAME')
 
-    return render_template('table.html', 
-    SignalChart=SignalChart,
-    **standard_args_table_page)
-
-
-@app.route('/changeSignalChart', methods=['POST'])
-@login_required
-def changeSignalChart():
-    """
-    Function called when "Confirm" buttin trigerred in "Signal Visualization" section
-    """
-    form = SearchForm(request.form)
-
-    standard_args_table_page = STD_FUNC_TABLE_PAGE()
-    validChartSignal = form.validChartSignal.data
-    validChartSignal = validChartSignal.replace(" ", "")
-
-    SignalChart = makeLinesSignal(tick=validChartSignal)
-
-    print('Changing signal chart.')
     return render_template('table.html',
-    SignalChart=SignalChart,
-    changeSig=True,
     **standard_args_table_page)
+
+@app.route('/portfolios')
+@login_required
+def portfolios():
+    return render_template('portfolios.html')
+
+
 
 
 
@@ -240,11 +207,6 @@ def tuplesToCSV(Tuples):
     return reReconstructedCSV
 
 
-@login_required
-@app.route("/sigAnalysis", methods=['GET'])
-def sigAnalysis():
-
-    return render_template('sigAnalysis.html')
 
 @app.route("/getCSV", methods=['GET'])
 @login_required
@@ -335,48 +297,145 @@ def search():
     
     return render_template('search.html', form=form)
 
-
-
-@app.route('/charts',methods=['GET', 'POST'])
-@login_required
-def charts():
-    form = SearchForm(request.form)
-    line = create_lineChart()
-
-    return render_template('charts.html', form=form, plot=line, tick='PLUG')
-
-
-@app.route('/submit', methods=['POST'])
-@login_required
-def getUserInput():
-    """
-    User input for charts table
-    """
-    form = SearchForm(request.form)
-    text = form.stock.data
-    processed_text = ""
-    processed_text = text.upper()
-    stocksList = list(pd.read_csv('utils/stocks_list.csv').iloc[:, 1])
-
-    if not processed_text or processed_text not in stocksList:
-        empty = True
-        return render_template('charts.html', form=form, empty=empty)
-    else:
-        line = create_lineChart(tick=processed_text)
-        empty = False
-        return render_template('charts.html', form=form, \
-        plot=line,tick=processed_text)
         
 
-
-
-@app.route('/infraHealth')
+@app.route('/api/fetchSignalChartJsonData')
 @login_required
-def infraHealth():
-    return render_template('infraHealth.html', health=True)
+def makeLinesSignal():
+    # http://127.0.0.1:5000/api/fetchSignalChartJsonData
+    tick = request.args["tick"]
+
+    if tick == '':
+        tick='AXR'
+
+    print(f"tick recieved: {tick}")
+    # You can have tick validation before moving ahead, if it's invalid tick then return error on UI
+
+    qu = f"SELECT t.*,t2.Gap FROM\
+    (SELECT * FROM signals.Signals_details\
+    WHERE Symbol='{tick}')t\
+    LEFT JOIN \
+    (SELECT * FROM marketdata.Technicals WHERE Ticker='{tick}')t2\
+    ON t2.Date = t.Date;"
+
+    df = db_acc_obj.exc_query(db_name='signals', query=qu, \
+    retres=QuRetType.ALLASPD)
+
+    fig = make_subplots(rows=7, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.03,
+                        row_width=[0.15, 0.15, 0.15, 0.15, 0.15,0.15,0.30],
+                        specs=[[{"rowspan":2}],
+                        [None],
+                        [{}],
+                        [{}],
+                        [{}],
+                        [{}],
+                        [{}]
+                        ])
+    fig.add_trace(go.Candlestick(x=df.Date,open=df.Open,close=df.Close,low=df.Low,high=df.High),
+                row=1, col=1)
+
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    
+    fig.add_trace(go.Scatter(x=df.Date, y=df['long_mavg'], name='long_mvg 50',mode='lines',
+        line=dict(color='orange',dash='dash')),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['short_mavg'], name='short_mvg 10',mode='lines',
+        line=dict(color='royalblue')),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date[df.positions==1], y=df.short_mavg[df.positions==1], 
+    name='MA crossing',mode='markers', marker_symbol='triangle-up', marker_size=10, marker_color='blue'),
+                row=1, col=1)
+    
+    fig.add_trace(go.Scatter(x=df.Date[df.doubleSignal==1], y=df.short_mavg[df.doubleSignal==1], 
+    name='Double Signal',mode='markers', marker_symbol='triangle-up', marker_size=15, marker_color='green'),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['Aroon_Up'], name='Aroon Up', mode='lines',
+        line=dict(color='green')),
+                row=3, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['Aroon_Down'], name='Aroon Down', mode='lines',\
+        line=dict(color='red')),
+                row=3, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['Volume'], name='Volume', mode='lines',\
+        line=dict(color='purple')),
+                row=4, col=1)
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['diff_stock_bench'], name='diff_stock_bench', mode='lines',\
+        line=dict(color='purple')),
+                row=5, col=1)
+
+
+    fig.add_trace(go.Scatter(x=df.Date, y=df['rolling_mean_35'], name='rolling_mean_35', mode='lines',\
+        line=dict(color='red')),
+                row=6, col=1)
+    
+    fig.add_trace(go.Scatter(x=df.Date, y=df['RSI'], name='RSI', mode='lines',\
+        line=dict(color='Orange')),
+                row=7, col=1)
+
+    fig.update_yaxes(showline=False, linewidth=1,gridwidth=0.2, linecolor='grey', gridcolor='rgba(192,192,192,0.5)',zeroline=True,zerolinewidth=1,zerolinecolor='black')
+
+
+    fig.update_traces(line_width=1.5)
+    fig.update_layout(
+    title=f'Trend Reversal Detection for {tick}',
+    #width=1400,
+    height=1100,
+    plot_bgcolor='rgba(0,0,0,0)',
+    margin=dict(
+    autoexpand=False,
+    l=100,
+    r=20,
+    t=110,
+    ),
+    legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+        )
+    )
+    fig.update_yaxes(showline=False, linewidth=1,gridwidth=0.2, linecolor='grey', gridcolor='rgba(192,192,192,0.5)')
+
+
+    fig['layout']['xaxis6']['title']='Date'
+    fig['layout']['yaxis']['title']='Price'
+    fig['layout']['yaxis2']['title']='Aroon'
+    fig['layout']['yaxis3']['title']='Volume'
+    fig['layout']['yaxis4']['title']='diff_stock_bench'
+    fig['layout']['yaxis5']['title']='rolling_mean_35'
+    fig['layout']['yaxis6']['title']='RSI'
+
+
+    annotations = []
+
+    annotations.append(dict(xref='paper', yref='paper', x=0, y=-0.09,
+                              xanchor='left', yanchor='top',
+                              #text='Log scale is used for vol. to have better grasp incoming vol on smaller caps',
+                              font=dict(family='Arial',
+                                        size=12,
+                                        color='rgb(150,150,150)'),
+                              showarrow=False))
+    
+    
+    fig.update_layout(annotations=annotations)
+    
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+    # return {}
+
 
 
 
 if __name__ == '__main__':
     db_acc_obj = std_db_acc_obj()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
